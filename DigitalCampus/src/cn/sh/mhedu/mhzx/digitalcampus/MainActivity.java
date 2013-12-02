2faaaa80c95923f7a5be17695e607ed65b3b9827
seyajin@163.com <cenyaoyi@163.com>
2013-12-02 15:34:48 +0800

    main interface

--- DigitalCampus/src/cn/sh/mhedu/mhzx/digitalcampus/MainActivity.java
+++ DigitalCampus/src/cn/sh/mhedu/mhzx/digitalcampus/MainActivity.java
@@ -1,8 +1,9 @@
 package cn.sh.mhedu.mhzx.digitalcampus;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +11,7 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
 	}
 
 	@Override
@@ -17,6 +19,14 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	public void handleInformationSoftwareBtnClick(View view) {
+		// TODO
+	}
+	
+	public void handleEntryCourseBtnClick(View view) {
+		// TODO
 	}
 
 }
