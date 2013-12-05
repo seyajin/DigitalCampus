da89d4187bdcc71c482c56846e25692b9033123f
seyajin@163.com <cenyaoyi@163.com>
2013-12-05 13:18:07 +0800

    add button

--- DigitalCampus/gen/cn/sh/mhedu/mhzx/digitalcampus/R.java
+++ DigitalCampus/gen/cn/sh/mhedu/mhzx/digitalcampus/R.java
@@ -21,7 +21,10 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int button_blue=0x7f020000;
+        public static final int button_normal=0x7f020001;
+        public static final int button_pressed=0x7f020002;
+        public static final int ic_launcher=0x7f020003;
     }
     public static final class id {
         public static final int action_settings=0x7f080000;
@@ -35,7 +38,12 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
+        public static final int copyright=0x7f050007;
+        public static final int digital_campus=0x7f050004;
+        public static final int entry_course=0x7f050006;
+        public static final int information_software=0x7f050005;
+        public static final int shanghai_minhang_highschool=0x7f050002;
+        public static final int shanghai_minhang_highschool_pinyin=0x7f050003;
     }
     public static final class style {
         /** 
